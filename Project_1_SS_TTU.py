527b205a95e3daffdda73fdbb6cb467f2e03395c
clootch <tpwmustang@gmail.com>
2021-06-07 19:06:23 -0500

    Update Project_1_SS_TTU.py

---
+++ Project_1_SS_TTU.py
@@ -0,0 +1,14 @@
+import threading
+
+
+class Object:
+    def send():
+        pass
+    def recieve():
+        pass
+    def reply():
+        pass
+
+
+if __name__ == "__main__":
+    sender = threading.Thread()
