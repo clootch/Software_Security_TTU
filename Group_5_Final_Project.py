434603f66ffc4467e851651d74bad658c1a2b249
clootch <68715725+clootch@users.noreply.github.com>
2021-06-16 16:19:00 -0500

    Create Group_5_Final_Project.py

---
+++ Group_5_Final_Project.py
@@ -0,0 +1,65 @@
+import threading
+
+class Customer(threading.Thread):
+    def __init__(self):
+        pass
+    
+    def log_on(self):
+        pass
+
+    def log_out(self):
+        pass
+
+    def query_account(self):
+        pass
+
+    def transfer_funds(self):
+        pass
+
+    def view_profile(self):
+        pass
+
+    def query_stock(self):
+        pass
+
+    def buy_stock(self):
+        pass
+
+    def sell_stock(self):
+        pass
+
+class Bank_Teller(threading.Thread):
+    def __init__(self):
+        pass
+
+    def log_on(self):
+        pass
+
+    def log_out(self):
+        pass
+
+    def query_account(self):
+        pass
+
+    def withdraw_funds(self):
+        pass
+
+    def view_profile(self):
+        pass
+
+    def query_stock(self):
+        pass
+
+class Stock_Trading_System(threading.Thread):
+    def __init__(self):
+        pass
+
+    def buy_stock(self):
+        pass
+
+    def sell_stock(self):
+        pass
+
+
+
+
